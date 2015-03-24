272ba3ec3fe4c27bd89a93e9a847b708865ebfc7
zdgeorgiev <zddavko@gmail.com>
2015-03-24 19:03:04 +0200

    fixed

--- Core-Java/week1/1-DebuggingFun/DebuggingFun/test/com/hackbulgaria/corejava/FaultyProblem1Test.java
+++ Core-Java/week1/1-DebuggingFun/DebuggingFun/test/com/hackbulgaria/corejava/FaultyProblem1Test.java
@@ -14,12 +14,6 @@ public class FaultyProblem1Test {
         this.faultyProgram = new FaultyProblem1();
     }
     
-    //F5 -> IN!
-    //F6 
-    // |
-    // next line
-    //F7 <-- out
-    
     @Test
     public void test() {
         assertEquals(1, faultyProgram.getLargestPalindrome(2));
@@ -27,5 +21,4 @@ public class FaultyProblem1Test {
         assertEquals(121, faultyProgram.getLargestPalindrome(126));
         assertEquals(898, faultyProgram.getLargestPalindrome(908));
     }
-
 }
