8c40855d50e15a3295e70207601c4f41b2306268
zdgeorgiev <zddavko@gmail.com>
2015-06-24 19:27:24 +0300

    fixed

--- Algorithms/week3/1-BST/BST.java
+++ Algorithms/week3/1-BST/BST.java
@@ -9,7 +9,43 @@ public class BST {
 
     // Checks if a binary tree is a binary search tree.
     public boolean isBST(Node root) {
-        // ...
-        return false;
+        return BSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean BSTUtil(Node root, int minValue, int maxValue) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if (checkLeftSubTree(root.left, minValue, root.value) && checkRightSubTree(root.right, root.value, maxValue)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean checkLeftSubTree(Node node, int minValue, int maxValue) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.value < minValue || node.value > maxValue) {
+            return false;
+        }
+
+        return checkLeftSubTree(node.left, minValue, node.value) && checkRightSubTree(node.right, node.value, maxValue);
+    }
+
+    private boolean checkRightSubTree(Node node, int minValue, int maxValue) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.value <= minValue || node.value > maxValue) {
+            return false;
+        }
+
+        return checkLeftSubTree(node.left, minValue, node.value) && checkRightSubTree(node.right, node.value, maxValue);
     }
 }
