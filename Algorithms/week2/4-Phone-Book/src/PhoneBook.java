e9feab188e3a2c536de51528fa058a85ac95dae8
zdgeorgiev <zddavko@gmail.com>
2015-06-30 21:28:02 +0300

    fixed

---
+++ Algorithms/week2/4-Phone-Book/src/PhoneBook.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class PhoneBook {
+
+    public static class Contact {
+
+        public String name;
+        public int number;
+    }
+
+    // Find the names of people based on their phone numbers.
+    public List<String> lookupNames(List<Contact> phoneBook, List<Integer> numbers) {
+        return null;
+    }
+}
