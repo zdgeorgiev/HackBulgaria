e4eae962c071bf2c3658da8aa1094d8c3b8f95e5
zdgeorgiev <zddavko@gmail.com>
2015-07-11 13:43:25 +0300

    fixed

--- Algorithms/week2/6-K-Lists/KLists.java
+++ Algorithms/week2/6-K-Lists/KLists.java
@@ -1,14 +1,60 @@
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class KLists {
 
-  public class Node {
-    public int value;
-    public Node next;
-  }
+    private static StringBuilder output = new StringBuilder();
 
-  // Merge K sorted lists.
-  public Node merge(List<Node> lists) {
-    // ...
-  }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        int n = s.nextInt();
+
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i < n; i++) {
+
+            int currentNumber = s.nextInt();
+            ArrayList<Integer> currentList = new ArrayList<Integer>();
+
+            while (currentNumber != -1) {
+                currentList.add(currentNumber);
+                currentNumber = s.nextInt();
+            }
+
+            lists.add(currentList);
+        }
+
+        mergeArrays(lists);
+
+        System.out.println(output);
+    }
+
+    public static void mergeArrays(ArrayList<ArrayList<Integer>> arrays) {
+
+        PriorityQueue<LinkedList<Integer>> heap = new PriorityQueue<LinkedList<Integer>>(1000,
+                new Comparator<LinkedList<Integer>>() {
+
+                    public int compare(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+                        return Integer.compare(list1.peek(), list2.peek());
+                    }
+                });
+
+        for (ArrayList<Integer> a : arrays) {
+            heap.add(new LinkedList<Integer>(a));
+        }
+
+        while (heap.size() > 0) {
+            LinkedList<Integer> current = heap.poll();
+            Integer head = current.pop();
+            output.append(head + " ");
+            if (current.peekFirst() != null) {
+                heap.add(current);
+            }
+        }
+    }
 }
