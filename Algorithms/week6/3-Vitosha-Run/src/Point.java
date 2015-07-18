5f573ca172bdbdc8fb05af904eec1f9b179861dd
zdgeorgiev <zddavko@gmail.com>
2015-07-19 01:05:27 +0300

    fixed

---
+++ Algorithms/week6/3-Vitosha-Run/src/Point.java
@@ -0,0 +1,12 @@
+public class Point {
+
+    public int x;
+    public int y;
+    public int value;
+
+    public Point(int x, int y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+}
