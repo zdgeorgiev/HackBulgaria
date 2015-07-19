6e31913c0ae82f5d7201f59ad26004c7db9c42ca
zdgeorgiev <zddavko@gmail.com>
2015-07-19 22:06:31 +0300

    fixed

--- Algorithms/week4/2-Valid-Directories/ValidDirectories.java
+++ Algorithms/week4/2-Valid-Directories/ValidDirectories.java
@@ -1,52 +1,78 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class ValidDirectories {
 
-    private static int[] visited;
+    public static void main(String[] args) {
 
-    public boolean isValid(int[][] graph) {
+        Scanner s = new Scanner(System.in);
+        int count = s.nextInt();
 
-        Queue<Integer> queue = new LinkedList<Integer>();
+        ArrayList<Graph.Node> allNodes = new ArrayList<Graph.Node>();
+        for (int i = 0; i < count; i++) {
+            allNodes.add(new Graph.Node(""));
+        }
 
-        queue.add(0);
-        visited[0] = 1;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                int nextConnection = s.nextInt();
 
-        while (!queue.isEmpty()) {
-
-            int currentIndex = queue.poll();
-
-            for (int i = 0; i < graph[currentIndex].length; i++) {
-
-                if (graph[currentIndex][i] == 1) {
-                    if (visited[i] == 0) {
-                        queue.add(i);
-                        visited[i] = 1;
-                    } else {
-                        return false;
-                    }
+                if (nextConnection == 1) {
+                    allNodes.get(i).addEdge(allNodes.get(j));
                 }
             }
         }
 
-        return true;
-    }
+        // Graph.Node[] allNodes = { seven, five, three, eleven, eight, two,
+        // nine, ten };
+        // L <- Empty list that will contain the sorted elements
+        ArrayList<Graph.Node> L = new ArrayList<Graph.Node>();
 
-    public static void main(String[] agrs) {
-        ValidDirectories v = new ValidDirectories();
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-
-        int[][] graph = new int[n][n];
-        visited = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = s.nextInt();
+        // S <- Set of all nodes with no incoming edges
+        HashSet<Graph.Node> S = new HashSet<Graph.Node>();
+        for (Graph.Node n : allNodes) {
+            if (n.inEdges.size() == 0) {
+                S.add(n);
             }
         }
 
-        System.out.println(v.isValid(graph));
+        // while S is non-empty do
+        while (!S.isEmpty()) {
+            // remove a node n from S
+            Graph.Node n = S.iterator().next();
+            S.remove(n);
+
+            // insert n into L
+            L.add(n);
+
+            // for each node m with an edge e from n to m do
+            for (Iterator<Graph.Edge> it = n.outEdges.iterator(); it.hasNext();) {
+                // remove edge e from the graph
+                Graph.Edge e = it.next();
+                Graph.Node m = e.to;
+                it.remove();// Remove edge from n
+                m.inEdges.remove(e);// Remove edge from m
+
+                // if m has no other incoming edges then insert m into S
+                if (m.inEdges.isEmpty()) {
+                    S.add(m);
+                }
+            }
+        }
+        // Check to see if all edges are removed
+        boolean cycle = false;
+        for (Graph.Node n : allNodes) {
+            if (!n.inEdges.isEmpty()) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
     }
 }
