0f5449c31a4e69907a467fb89e3d5f433c97c968
zdgeorgiev <zddavko@gmail.com>
2015-07-19 20:21:58 +0300

    fixed

--- Algorithms/week4/2-Valid-Directories/ValidDirectories.java
+++ Algorithms/week4/2-Valid-Directories/ValidDirectories.java
@@ -1,16 +1,17 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class ValidDirectories {
 
-    private ArrayList<Integer> visited = new ArrayList<Integer>();
+    private static int[] visited;
 
     public boolean isValid(int[][] graph) {
 
         Queue<Integer> queue = new LinkedList<Integer>();
 
         queue.add(0);
+        visited[0] = 1;
 
         while (!queue.isEmpty()) {
 
@@ -18,13 +19,13 @@ public class ValidDirectories {
 
             for (int i = 0; i < graph[currentIndex].length; i++) {
 
-                if (!this.visited.contains(graph[currentIndex][i])) {
-                    if (graph[currentIndex][i] == 1) {
+                if (graph[currentIndex][i] == 1) {
+                    if (visited[i] == 0) {
                         queue.add(i);
-                        this.visited.add(i);
+                        visited[i] = 1;
+                    } else {
+                        return false;
                     }
-                } else {
-                    return false;
                 }
             }
         }
@@ -34,8 +35,17 @@ public class ValidDirectories {
 
     public static void main(String[] agrs) {
         ValidDirectories v = new ValidDirectories();
-        int[][] graph = new int[][] { { 0, 1, 0, 1, 0, 2 }, { 0, 0, 2, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
-                { 1, 0, 0, 0, 2, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int[][] graph = new int[n][n];
+        visited = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = s.nextInt();
+            }
+        }
 
         System.out.println(v.isValid(graph));
     }
